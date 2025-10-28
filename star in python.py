50a313d86b3ea22b0b13a8f81700dc69168ec3c4
MohammadReza <baloochmohammadreza@gmail.com>
2025-10-28 16:16:52 +0330

    * in function argument makes a tuple of additional argument and ** makes a dict of kw additional arguments

--- star in python.py
+++ star in python.py
@@ -3,3 +3,25 @@ print(a, b, type(b))
 
 a, *b = [1, 2, 3, 4, 5, 6, 7]
 print(a, b, type(b))
+
+name = "akbar"
+
+
+def testfunc(a, *args, name="ali", **kwargs):
+    print(
+        a,
+        type(a),
+        "\n",
+        args,
+        type(args),
+        "\n",
+        name,
+        type(name),
+        "\n",
+        kwargs,
+        type(kwargs),
+        "\n",
+    )
+
+
+testfunc(26, 23, 15, 67, 9, 23, "90", name="reza", loc="tehran", country="germany")
